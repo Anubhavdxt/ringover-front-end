4715feec1a49d51d62218599d7df56751877492a
Anubhav Dixit <anubhavdxt46@gmail.com>
2022-07-22 21:09:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Contact from "./Components/Contact/Contact";
+import Journey from "./Container/Journey/Journey";
+import Store from "./Container/Store/Store";
+import Team from "./Container/Team/Team";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Store />} />
+        <Route path="/home" element={<Store />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/journey" element={<Journey />} />
+      </Routes>
     </div>
   );
 }
